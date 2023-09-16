81bac42a421838541c1028149674b4659df46b7b
HyegyoJung <junghk0115@gmail.com>
2023-09-17 01:27:53 +0900

    [feat] photo picker 오픈 구현

--- app/src/main/java/com/example/fooda/DailyWriteActivity.kt
+++ app/src/main/java/com/example/fooda/DailyWriteActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fooda
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -16,6 +19,23 @@ class DailyWriteActivity : AppCompatActivity() {
 
         binding = ActivityDailyWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val pickMultipleMedia =
+            registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+                // Callback is invoked after the user selects media items or closes the
+                // photo picker.
+                if (uris.isNotEmpty()) {
+                    Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
+                    Log.d( "PhotoPicker", "${uris}")
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+
+
+        binding.imageAddButton.setOnClickListener{ pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))}
+
 
 
     }
